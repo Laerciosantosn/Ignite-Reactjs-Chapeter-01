c6f293860ccda62d9c7ca805a32d051bc795e747
Laercio Santos <laerciosantosn@gmail.com>
2021-04-01 10:45:39 -0300

    Primeiro componente React

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,33 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>List of repositories</h1>
+      <ul>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Access repository
+          </a>
+        </li> 
+
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Access repository
+          </a>
+        </li> 
+        
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Access repository
+          </a>
+        </li> 
+
+      </ul>
+    </section>
+  )
+}
