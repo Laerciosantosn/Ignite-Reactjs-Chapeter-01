359c1a0f1ab30f5d00d8ef7988e6170fb1f78ff7
Laercio Santos <laerciosantosn@gmail.com>
2021-04-01 18:43:53 -0300

    Componentes com typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,10 +3,15 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useEffect, useState } from "react";
 
-// https://api.github.com/users/Laerciosantosn/repos
+interface Repository {
+  id: number;
+  name: string; 
+  description: string; 
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/users/Laerciosantosn/repos')
